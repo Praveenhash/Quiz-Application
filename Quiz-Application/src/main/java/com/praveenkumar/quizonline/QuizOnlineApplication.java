5bb6ebe6ceef177d88561287a17c61f7fe7d7a7f
Praveenhash <praveenkumarsulikeri@gmail.com>
2025-08-22 22:00:57 +0530

    Committing necessary changes done

--- Quiz-Application/src/main/java/com/praveenkumar/quizonline/QuizOnlineApplication.java
+++ Quiz-Application/src/main/java/com/praveenkumar/quizonline/QuizOnlineApplication.java
@@ -1,4 +1,4 @@
-package com.dailycodework.quizonline;
+package com.praveenkumar.quizonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuizOnlineApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(QuizOnlineApplication.class, args);
     }
 
